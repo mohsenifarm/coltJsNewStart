a24256626cf89577644dffe10f12ebda5ebfb5be
Mohammad Javad Mohseni Far <mohammadjavadmohsenifar@Mohammads-MBP.lan1>
2024-06-21 12:16:36 -0700

    domIdSelector

---
+++ dom/app.js
@@ -0,0 +1,8 @@
+// DOM stand for Documaent Object Model 
+//selecting
+//1.getElementById
+let banner = document.getElementById("banner");
+// console.log(banner)
+let toc = document.getElementById('toc');
+// console.log(toc)
+// console.dir(toc)
